c396351e3965031b2065424cad67d4ef3cc579e3
bhavesh <bhaveshgupta41@gmail.com>
2023-01-16 20:15:25 +0530

    added post method

--- index.js
+++ index.js
@@ -4,11 +4,15 @@ const app = express();
 app.use(cors())
 const port = process.env.PORT || 3000;
 const appiData = require("./data.json")
+const postData = require("./postdata.json")
 app.get("/", (req,res) => {
     res.send("helooo guuuyyyjjjjjjj")
 })
 app.get("/list", (req,res) => {
     res.send(appiData)
+})
+app.post("/show", (req,res) => {
+    res.json(postData)
 })
 app.listen(port, () => {
     console.log("i am live baby");
